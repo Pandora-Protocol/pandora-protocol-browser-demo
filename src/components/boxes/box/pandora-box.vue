9a5d48e0f85ccf11bfba73de3bf22d52b346fd39
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2020-07-27 21:41:54 +0300

    show streams status individually.

--- src/components/boxes/box/pandora-box.vue
+++ src/components/boxes/box/pandora-box.vue
@@ -5,7 +5,14 @@
         <span>name: {{ this.box.name }}</span>
         <span>description: {{ this.box.description }}</span>
 
-        <textarea type="text" rows="4" cols="50" v-model="streams" />
+        <span v-for="(stream, i) in streams"
+              :key="`box_stream_${i}`">
+            {{stream.path}}
+            {{stream.type}}
+            {{stream.size}} bytes
+            {{stream.isDone}}
+            {{stream.percent}} %
+        </span>
 
         <span v-if="enableStreamliner" @click="streamline" style="cursor: pointer" > >>>> </span>
 
@@ -15,6 +22,7 @@
 </template>
 
 <script>
+import Vue from 'vue';
 export default {
 
     props: {
@@ -25,21 +33,12 @@ export default {
     data(){
         return {
             percent: 0,
+            streams: [],
         }
     },
 
     computed:{
 
-        streams(){
-
-            let out = '';
-            for (const stream of this.box.streams){
-                const s =  stream.path+ '  '+ stream.type+'   '+ stream.size;
-                out += s + '\n';
-            }
-
-            return out;
-        }
 
     },
 
@@ -59,15 +58,63 @@ export default {
 
             });
 
+        },
+
+        getStreams(){
+
+            const out = [];
+            for (let i=0; i < this.box.streams.length; i++) {
+                const stream = this.box.streams[i];
+                out.push( this.streamToInfoObject(stream) );
+            }
+
+            return out;
+        },
+
+        streamToInfoObject(stream){
+
+            let percent;
+            if (stream.type === 0){
+                percent = (stream.chunksCount - stream.statusUndoneChunks.length) / (stream.chunksCount || 1) * 100;
+            } else {
+                percent = stream.isDone ? 100 : 0;
+            }
+
+            return {
+                hash: stream.hash.toString('hex'),
+                path: stream.path,
+                type: stream.type,
+                size: stream.size,
+                isDone: stream.isDone,
+                percent,
+            }
         }
 
     },
 
     mounted(){
+
+        this.streams = this.getStreams();
         this.percent = this.box.chunksTotalAvailable / (this.box.chunksTotal || 1) * 100;
         this.box.on('chunks/total-available', ({chunksTotalAvailable, chunksTotal} ) => {
             this.percent = chunksTotalAvailable / ( chunksTotal || 1) * 100;
         } )
+
+        this.box.on('stream-chunk/done', ({stream})=>{
+
+            for (let i=0; i < this.streams.length; i++)
+                if (this.streams[i].path === stream.path)
+                    Vue.set(this.streams, i, this.streamToInfoObject(stream) );
+        })
+
+        this.box.on('stream/done', ({stream}) => {
+
+            for (let i=0; i < this.streams.length; i++)
+                if (this.streams[i].path === stream.path)
+                    Vue.set(this.streams, i, this.streamToInfoObject(stream) );
+
+        });
+
     }
 
 }
